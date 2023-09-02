16d28ab580f2b8010a9ebaf99c831147fe61dd59
Bharathi <saddabharathi9492@gmail.com>
2023-09-02 10:50:27 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Shop from './Cart';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Shop></Shop>
     </div>
   );
 }
