da05437aef08a6b1d8479edaab41ba69bbfadbf9
Bharathi <saddabharathi9492@gmail.com>
2023-09-15 08:43:01 +0530

    reduxapi

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
         <div className="d-flex flex-wrap">
           {
           fakestore.products.map((a)=>{
-          return  <div  className="card shadow-lg p-3 mb-5 bg-white rounded m-4 bg-info-subtle text-emphasis-info" style={{width: '14rem'}}>
-                    
+          return  <div  className="card shadow-lg p-3 mb-5 bg-white rounded m-4 bg-info-subtle text-emphasis-info" style={{width: '14rem'}}>          
           <div className="card-body d-flex flex-column justify-content-between"  >
           <img src={a.image} style={{height:'160px',width:'160px'}} alt="Card image cap"/>
             <h5 className="card-title">{a.title}</h5>
